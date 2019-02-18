e4bdd599890762c2d4eec4feddd373ef9939c2cb
Rusu83 <35503591+Rusu83@users.noreply.github.com>
2019-02-18 10:54:49 +0200

    Add files via upload

---
+++ bule/sketch.js
@@ -0,0 +1,28 @@
+var i,x=[],y=[],s=[],v=[];
+
+function setup() {
+  createCanvas(window.innerWidth,window.innerHeight);
+  for(i=0;i<100;i++){
+    x[i]=random(width);
+    y[i]=height/10*11;
+    s[i]=random(width/10);
+    v[i]=random(0,2);
+  }
+}
+
+function draw() {
+  background(15,50,50);
+  fill(200,200,255,50);
+  stroke(220,220,255,50);
+  for(i=0;i<100;i++){
+    //x[i]+=random(-width/50,width/50);
+    y[i]-=v[i];
+    if(y[i]<0){
+      x[i]=random(width);
+      y[i]=height/10*11;
+      s[i]=random(width/10);
+      v[i]=random(0,2);
+    }
+    ellipse(x[i],y[i],s[i],s[i]);
+  }
+}
